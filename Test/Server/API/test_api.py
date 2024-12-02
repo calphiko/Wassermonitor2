0ec14b99d3e3d9b777cb8d84e8e909c839d3ff08
calle <cp@koppen.me>
2024-12-02 21:50:37 +0100

    initial

---
+++ Test/Server/API/test_api.py
@@ -0,0 +1,34 @@
+# Testsuite for API
+
+from datetime import datetime
+import unittest
+from requests import post
+import pytz
+import json
+
+test_url = "http://127.0.0.1:8000/insert/"
+
+test_meas_dict = {
+    'datetime': datetime.now(tz=pytz.utc).isoformat(),
+    'pi_name': 'measurement_pi_1',
+    'sensor_id': 5,
+    'values': [
+        1.34562,
+        1.34561,
+        1.34562,
+        1.34563,
+        1.345625
+    ]
+}
+
+class Test_api_request(unittest.TestCase):
+    def test_json_to_database(self):
+
+        print (f"Sended {test_meas_dict} to database")
+        r = post(test_url, json=json.dumps(test_meas_dict))
+
+        print(f"Answer was: {r}")
+
+
+if __name__ == '__main__':
+    unittest.main()
