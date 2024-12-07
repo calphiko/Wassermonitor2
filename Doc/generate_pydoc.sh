d2c59ad331a5c406dfcf4d0ead6bb1bc989832ab
calle <cp@koppen.me>
2024-12-07 11:27:21 +0100

    bugfix
    changed pydoc -w main.py to pydoc -w main

--- Doc/generate_pydoc.sh
+++ Doc/generate_pydoc.sh
@@ -2,17 +2,17 @@
 source ../.venv/bin/activate
 
 cd ../Pi/
-pydoc3 main.py
+pydoc -w main
 mv main.html ../Doc/content/10_Functions_Reference/pi.html
 
 cd ../Server/API/
 
-pydoc3 main.py
+pydoc -w main
 mv main.html ../../Doc/content/10_Functions_Reference/api.html
 
 cd ../Warningbot/
 
-pydoc3 main.py
+pydoc -w main
 mv main.html ../../Doc/content/10_Functions_Reference/warning_bot.html
 
 cd ../../
