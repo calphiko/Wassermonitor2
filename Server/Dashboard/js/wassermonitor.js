6fdde85090d314d8c24ed3ac15f74a94ea30b6db
calle <cp@koppen.me>
2024-12-10 21:54:31 +0100

    delete

--- Server/Dashboard/js/wassermonitor.js
+++ Server/Dashboard/js/wassermonitor.js
@@ -1,3 +1,5 @@
+import * as echarts from 'echarts';
+
 async function getData() {
     const url = 'http://127.0.0.1:8012/get_latest/';
     const token = 'secret_token'
@@ -21,4 +23,105 @@ async function getData() {
     }
 }
 
-window.getData = getData;
+async function getFillPlot() {
+    const data = await getData();
+
+    const xAxisData = data.map(item => 'sensor_id');
+    const xAxisData = data.map(item => 'value');
+
+    const chartDom = document.getElementById('main');
+    const FillPlot = echarts.init(chartDom);
+
+    const option = option = {
+        title: {
+        text: 'Wassermonitor',
+        subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
+    },
+    xAxis: {
+        data: xAxisData,
+        axisLabel: {
+            inside: true,
+            color: '#fff'
+        },
+        axisTick: {
+            show: false
+        },
+        axisLine: {
+            show: false
+        },
+        z: 10
+    },
+    yAxis: {
+        axisLine: {
+        show: false
+        },
+        axisTick: {
+            show: false
+        },
+        axisLabel: {
+            color: '#888'
+        },
+        type: 'value',
+        max: 150,
+        min: 0,
+    },
+    dataZoom: [
+        {
+            type: 'inside'
+        }
+    ],
+    series: [
+        {
+            type: 'bar',
+            showBackground: false,
+            itemStyle: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                    { offset: 0, color: '#83bff6' },
+                    { offset: 0.5, color: '#188df0' },
+                    { offset: 1, color: '#188df0' }
+                ])
+            },
+            data: data,
+            barWidth: '90%',
+            markLine: {
+            data: [
+                {
+                  yAxis: 60,
+                  lineStyle: {
+                    color:'red',
+                    type: 'dashed'
+                  },
+                  label:{
+                    formatter: 'Alarmschwelle'
+                  },
+                },
+                {
+                  yAxis: 90,
+                  lineStyle: {
+                    color:'orange',
+                    type: 'dashed'
+                  },
+                  label:{
+                    formatter: 'Warnschwelle'
+                  },
+                },
+
+              ]
+          },
+        },
+      ]
+
+
+    };
+    // Enable data zoom when user click bar.
+    const zoomSize = 4;
+    myChart.on('click', function (params) {
+      console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
+      myChart.dispatchAction({
+        type: 'dataZoom',
+        startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+        endValue:
+          dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+      });
+    });
+}
