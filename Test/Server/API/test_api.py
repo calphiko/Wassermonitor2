7d4358c81d0121837bcf9fbcb7edc4244a19bd49
calle <cp@koppen.me>
2024-12-10 19:24:22 +0100

    hided debug data

--- Test/Server/API/test_api.py
+++ Test/Server/API/test_api.py
@@ -78,7 +78,7 @@ class Test_api_request(unittest.TestCase):
             'dt_end': datetime(year=2024, month=4, day=12, hour=23, minute=59, second=59).isoformat()
         }
         r = post(test_url_get, json=json.dumps(test_request_dict), headers=headers)
-        print (r.json())
+        #print (r.json())
         assert r.status_code == 200, "Unexpected status code: " + str(r.status_code)
 
     def test_get_latest_data_from_database(self):
@@ -86,7 +86,7 @@ class Test_api_request(unittest.TestCase):
             "Authorization": f"Bearer {test_token}"
         }
         r = post(test_url_get_latest, headers=headers)
-        print (r.json())
+        #print (r.json())
         assert r.status_code == 200, "Unexpected status code: " + str(r.status_code)
 
 if __name__ == '__main__':
