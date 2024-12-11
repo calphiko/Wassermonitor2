75c0b6aa12e21f780b50c5473d400c2a13b916be
calle <cp@koppen.me>
2024-12-11 17:04:51 +0100

    assing status to api response

--- Server/API/main.py
+++ Server/API/main.py
@@ -39,6 +39,7 @@ import configparser
 import json
 import time
 from datetime import datetime
+import pandas as pd
 
 config_file = "../config.cfg"
 
@@ -183,16 +184,29 @@ def request_measurement_data(request_dict):
     }
     return JSONResponse(content=data_json)
 
+def assign_color(value):
+    if value < float(config['threshold']['alarm']):
+        #return "red"
+        return 'alarm'
+    elif value < float(config['threshold']['warning']):
+        #return "orange"
+        return 'warning'
+    else:
+        #return "#0055bb"
+        return 'normal'
+
 def request_last_measurements():
     data = dbu.get_last_meas_data_from_sqlite_db(
         config['database']
     )
+    data['color'] = data['value'].apply(assign_color)
     #print(data)
     data_json = {
         'dt':data['dt'].tolist(),
         'sensor_id': data['sensor_id'].tolist(),
         'pi_name': data['pi_name'].tolist(),
         'value': data['value'].tolist(),
+        'color': data['color'].tolist(),
     }
     return JSONResponse(content=data_json)
 
