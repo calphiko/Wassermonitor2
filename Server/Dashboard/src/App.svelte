19e36c29e46e8af416c585bf9cf47198283d9ede
calle <cp@koppen.me>
2024-12-11 09:24:56 +0100

    changed to Svelte

--- Server/Dashboard/src/App.svelte
+++ Server/Dashboard/src/App.svelte
@@ -26,96 +26,15 @@
     });
 
     function createFillPlot() {
+        const chartDom = document.getElementById('main');
         if (chartDom) {
-            const chartDom = document.getElementById('main');
+
             const myChart = echarts.init(chartDom);
 
             const xData = data.map(item => item.sensor_id)
             const yData = data.map(item => item.value)
 
-            const option = {
-              title: {
-                text: 'Wassermonitor',
-                subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
-              },
-              xAxis: {
-                data: xData,
-                axisLabel: {
-                  inside: true,
-                  color: '#fff'
-                },
-                axisTick: {
-                  show: false
-                },
-                axisLine: {
-                  show: false
-                },
-                z: 10
-              },
-              yAxis: {
-                axisLine: {
-                  show: false
-                },
-                axisTick: {
-                  show: false
-                },
-                axisLabel: {
-                  color: '#888'
-                },
-                type: 'value',
-                max: 150,
-                min: 0,
-              },
-              dataZoom: [
-                {
-                  type: 'inside'
-                }
-              ],
-              series: [
-                {
-                  type: 'bar',
-                  showBackground: false,
-                  itemStyle: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                      { offset: 0, color: '#83bff6' },
-                      { offset: 0.5, color: '#188df0' },
-                      { offset: 1, color: '#188df0' }
-                    ])
-                  },
 
-                  data: yData,
-                  barWidth: '90%',
-                  markLine: {
-                    data: [
-                        {
-                          yAxis: 60,
-                          lineStyle: {
-                            color:'red',
-                            type: 'dashed'
-                          },
-                          label:{
-                            formatter: 'Alarmschwelle'
-                          },
-                        },
-                        {
-                          yAxis: 90,
-                          lineStyle: {
-                            color:'orange',
-                            type: 'dashed'
-                          },
-                          label:{
-                            formatter: 'Warnschwelle'
-                          },
-                        },
-
-                      ]
-                  },
-                },
-              ]
-
-
-            };
-            myChart.setOption(option);
         } else {
             console.error ('Element with id "main" not found!');
         }
