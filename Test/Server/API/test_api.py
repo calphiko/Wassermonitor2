0dccc18ae2424a23ceda1120f07d8c5fe5098b32
calle <cp@koppen.me>
2024-12-28 14:27:41 +0100

    added data signing to first test

--- Test/Server/API/test_api.py
+++ Test/Server/API/test_api.py
@@ -6,24 +6,55 @@ from requests import post
 import pytz
 import json
 from data_for_api_test import *
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+import base64
 
 test_url = "http://127.0.0.1:8012/insert/"
 test_url_get = "http://127.0.0.1:8012/get/"
 test_url_get_latest = "http://127.0.0.1:8012/get_latest/"
 test_token = 'secret_token'
+test_api_psk_path = "../../../Pi/.wassermonitor"
+
+def read_priv_key(psk_path):
+    with open(f"{psk_path}/private_key.pem", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None  # Falls der Schlüssel passwortgeschützt ist, gib hier das Passwort an.
+        )
+    return private_key
+def sign_data_w_priv_key(data, psk_path):
+    private_key = read_priv_key(psk_path)
+    message = json.dumps(data).encode("utf-8")
+    signature = private_key.sign(
+        message,
+        padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
+        hashes.SHA256()
+    )
+    payload = {
+        "client_id": data['meas_point'],
+        "data": data,
+        "signature": base64.b64encode(signature).decode("utf-8")
+    }
+    print (payload)
+    return payload
+
 
 class Test_api_request(unittest.TestCase):
     def test_json_to_database(self):
         headers = {
             "Authorization": f"Bearer {test_token}"
         }
-        r_list = []
         for test_meas_dict in test_meas_dict_list:
             test_meas_dict['datetime'] = datetime.now(tz=pytz.utc).isoformat()
+            signed_test_meas_dict = sign_data_w_priv_key(test_meas_dict, test_api_psk_path)
             print (f"Sended {test_meas_dict} to database")
-            r = post(test_url, json=json.dumps(test_meas_dict), headers=headers)
+            r = post(test_url, json=json.dumps(signed_test_meas_dict), headers=headers)
             #r = post(test_url, json=test_meas_dict, headers=headers)
-            assert r.status_code == 200, "Unexpected status code: " + str(r.status_code)
+            if test_meas_dict["meas_point"] == "raspi1":
+                assert r.status_code == 200, "Unexpected status code: " + str(r.status_code)
+            else:
+                assert r.status_code == 401, "Unexpected status code: " + str(r.status_code)
 
     def test_json_to_database_w_wrong_json(self):
         print(f"Sended {test_meas_dict_fail} to database with wrong json")
