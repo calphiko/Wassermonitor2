c5a5bf5d325d438e92e441da6eb45d685b2fa08f
dekoppca <c.koppen@pmdtec.com>
2025-01-17 13:14:40 +0100

    fix:
    small changes for buildung svelte

--- Server/Dashboard/wassermonitor-dash/svelte.config.js
+++ Server/Dashboard/wassermonitor-dash/svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    extensions: ['.svelte'],
+    //extensions: ['.svelte'],
     preprocess: vitePreprocess(),
 
 	kit: {
