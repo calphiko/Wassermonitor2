97ead4a8b08ac79ce5608992c695451fe5fd5985
calle <cp@koppen.me>
2025-01-19 10:25:58 +0100

    fix:
    changed comments to english

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Start FastAPI im Hintergrund
+# Start FastAPI in background
 cd API
 #uvicorn main:app --host 0.0.0.0 --port 8000 &
 python3 main.py &
 
-# Start nginx im Vordergrun
+# Start nginx
 cd ..
 nginx -g "daemon off;"
