a44da7663fcfded0ba59aad9739b12f164de67c0
dekoppca <c.koppen@pmdtec.com>
2025-01-23 15:54:19 +0100

    added features:
    - configure system daemon for datascrawler.py
    - configure cronjob for datatransmitter.py

--- Pi/setup.sh
+++ Pi/setup.sh
@@ -59,4 +59,77 @@ python3 configure_sensors.py
 # GENERATE KEY PAIR FOR DATA SIGNING
 python3 generate_key_pair.py
 
+# INSTALL datacrawler AS SYSTEM DAEMON
+echo ""
+echo "CRAETE SYSTEMD DAEMON FOR DATASCRAWLER"
+SERVICE_USER="wm2"
+SERVICE_NAME="wassermonitor_datascrawler"
+PYTHON_PROGRAMM="datascrawler.py"
+CURRENT_DIR=$(pwd)
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
+# CREATE NEW USER FOR DAEMON
+if ! id -u "$SERVICE_USER" >/dev/null 2>&1; then
+    echo "Create new user: $SERVICE_USER"
+    sudo useradd --system --no-create-home --shell /usr/sbin/nologin "$SERVICE_USER"
+else
+    echo "User $SERVICE_USER already exists."
+fi
+
+# CREATE SERVICE FILE
+echo "Create systemd-service file: $SERVICE_FILE"
+sudo bash -c "cat > $SERVICE_FILE" <<EOL
+[Unit]
+Description=$SERVICE_NAME
+After=network.target
+
+[Service]
+User=$SERVICE_USER
+WorkingDirectory=$CURRENT_DIR
+ExecStart=$CURRENT_DIR/.venv/bin/python3 $CURRENT_DIR/$PYTHON_PROGRAM
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+
+echo "Load and enable daemon"
+sudo systemctl daemon-reload
+sudo systemctl enable "$SERVICE_NAME"
+sudo systemctl start "$SERVICE_NAME"
+
+# INSTALL datatransmitter AS CRON JOB
+echo ""
+echo "CREATE CRONJOB FOR SUBMITTING THE DATA"
+
+CRON_SCRIPT="datatransmitter.py"
+CRON_FILE="/var/spool/cron/crontabs/$SERVICE_USER"
+
+echo "CRON JOB THAT RUNS EVERY 5 MINUTES..."
+CRON_ENTRY="*/5 * * * * $CURRENT_DIR/.venv/bin/python3 $CURRENT_DIR/$PYTHON_SCRIPT"
+
+# Sicherstellen, dass die Crontab-Datei für den Benutzer existiert
+if [ ! -f "$CRON_FILE" ]; then
+    echo "Create new crontab for user $SERVICE_USER"
+    sudo touch "$CRON_FILE"
+    sudo chmod 600 "$CRON_FILE"
+    sudo chown "$SERVICE_USER":"$SERVICE_USER" "$CRON_FILE"
+fi
+
+# Cronjob hinzufügen, falls er noch nicht existiert
+if sudo crontab -u "$SERVICE_USER" -l 2>/dev/null | grep -qF "$CRON_ENTRY"; then
+    echo "Cronjob already exists."
+else
+    (sudo crontab -u "$SERVICE_USER" -l 2>/dev/null; echo "$CRON_ENTRY") | sudo crontab -u "$SERVICE_USER" -
+    echo "Cronjob successfully added."
+fi
+
+# Status überprüfen
+echo "Current Cronjobs $SERVICE_USER:"
+sudo crontab -u "$SERVICE_USER" -l
+
+echo ""
+echo "For uploading data to the API, please add this public key to your APIs authorized-keys file (Consider Server documentation)."
+cat $CURRENT_DIR/.psk/public_key.rsa
