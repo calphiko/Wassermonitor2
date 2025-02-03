bb3b99776414b1725adc9d3cbea8bc3b5aefc83e
dekoppca <c.koppen@pmdtec.com>
2025-02-03 11:12:21 +0100

    layout fix:
    - firstlinecolor if darkmode

--- Server/Dashboard/wassermonitor-dash/src/routes/charts.js
+++ Server/Dashboard/wassermonitor-dash/src/routes/charts.js
@@ -38,7 +38,7 @@ function reInitEchart(name, divName, charts, plotTheme, plotThemeDark) {
 
     const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     const theme = isDarkMode ? plotThemeDark : plotTheme;
-    firstLineColor = isDarkMode ? 'darkblue' : '#3F83F8';
+    firstLineColor = isDarkMode ? 'lightblue' : '#3F83F8';
     plotBackGround = isDarkMode ? 'transparent':'transparent'
     // MAYBE DELETABLE
     console.log("reinit charts with theme:", theme);
